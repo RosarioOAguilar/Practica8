5eb71d31fb338b449d6f3fc77d5c34e21456d43f
RosarioOAguilar <rosario.aguilar@live.com>
2013-12-11 20:33:45 -0600

    Practica8

---
+++ conexion.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace practica8
+{
+	
+	public class conexion
+	{
+		protected MySqlConnection myConnection;
+		public conexion()
+		{
+		}
+		
+		protected void abrirConexion(){
+			string connectionString =
+				"Server=localhost;" +
+				"Database=registro;" +
+				"User ID=root;" +
+				"Password=pifus1234567890;" +
+				"Pooling=false;";
+			this.myConnection = new MySqlConnection(connectionString);
+			this.myConnection.Open();
+		}
+		
+		protected void cerrarConexion(){
+			this.myConnection.Close();
+			this.myConnection = null;
+			
+		}
+		
+	}
+}
---
+++ registro.cs
@@ -0,0 +1,85 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace practica8
+{
+	
+	public class registro : conexion
+	{
+		public registro()
+		{
+			
+		}
+		public void mostrarconexion(){
+			this.abrirConexion();
+			MySqlCommand myCommand = new MySqlCommand(this.querySelect(),
+			                                          myConnection);
+			MySqlDataReader myReader = myCommand.ExecuteReader();
+			while (myReader.Read()){
+				string id = myReader["id"].ToString();
+				string nombre = myReader["Nombre"].ToString();
+				string codigo = myReader["Codigo"].ToString();
+				string telefono = myReader["Telefono"].ToString();
+				string email = myReader["Email"].ToString();
+				Console.WriteLine("ID: " + id +
+				                  " Nombre: " + nombre +
+				                  " Codigo: " + codigo +
+				                  " Telefono:" + telefono +
+				                  " Email:" + email);
+			}
+			
+			myReader.Close();
+			myReader = null;
+			myCommand.Dispose();
+			myCommand = null;
+			this.cerrarConexion();
+		}
+		public void agregarregistro(string nombre, string codigo, string telefono, string email){
+			this.abrirConexion();
+			string sql="INSERT INTO `datos`(`nombre`, `codigo`, `telefono`, `email`) VALUES ('"+nombre+"','"+codigo+"','"+telefono+"','"+email+"')";
+			this.ejecutarComando(sql);
+			this.cerrarConexion();
+			
+		}
+		public void editarRegistro(string id, string nombre,string codigo, string telefono, string email){
+			
+			Console.WriteLine("Seguro que desea Editarlo");
+	 			Console.WriteLine ("n=No, s=SI");
+	 			string opc = Console.ReadLine();
+	 			      if (opc!="n"){
+	 				
+			this.abrirConexion();
+			string sql = "UPDATE `datos` SET `nombre`='" + nombre + "',`codigo`='" + codigo + "',`telefono`='" + telefono + "',`email`='" + email + "'  WHERE (`id`='" + id + "')";
+			this.ejecutarComando(sql);
+			this.cerrarConexion();
+			Console.WriteLine("Se edito con exito");
+		}
+		}
+		public void eliminarRegistroPorId(string id){
+			Console.WriteLine("desea eliminarlo");
+	 			Console.WriteLine ("n=No, s=SI");
+	 			string opc = Console.ReadLine();
+	 			      if (opc!="n"){
+			this.abrirConexion();
+			string sql = "DELETE FROM `datos` WHERE (`id`='" + id + "')";
+			this.ejecutarComando(sql);
+			this.cerrarConexion();
+			Console.WriteLine("Se elimino con exito");
+		}
+		
+		}
+		private int ejecutarComando(string sql){
+			MySqlCommand myCommand = new MySqlCommand(sql,this.myConnection);
+			int afectadas = myCommand.ExecuteNonQuery();
+			myCommand.Dispose();
+			myCommand = null;
+			return afectadas;
+		}
+		
+		
+		private string querySelect(){
+			return "SELECT * " +
+				"FROM  datos";
+		}
+	}
+}
